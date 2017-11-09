3499a04f73908927570113cdb2a6f152472501b8
ganzpranajaya <ganz.pranajaya@gmail.com>
2017-11-09 09:54:23 +0700

    tugas sesi 4
    function greet

---
+++ sesi4/latihan.js
@@ -0,0 +1,133 @@
+/*var Buah = ["Apel", "Anggur", "Mangga", "Pir", "Semangka", "Jambu", "Durian"]
+
+
+
+console.log(Buah[0]);
+console.log(Buah[4]);
+Buah[4] = "Dukuh";
+console.log(Buah[4]);
+
+
+var person = {
+    name: 'Gun Gun',
+    age: 17,
+    address: 'Sukabumi Dalam',
+    gadget: {
+        laptop:'MBP',
+        phone:'iphone7',
+    },
+    hobby: ['Baca','Tidur']
+};
+
+console.log(person.name);
+console.log(person.gadget.phone);
+
+var Quran = {"code":200,"status":"OK","data":{"number":1,"name":"\u0633\u0648\u0631\u0629 \u0627\u0644\u0641\u0627\u062a\u062d\u0629","englishName":"Al-Faatiha","englishNameTranslation":"The Opening","revelationType":"Meccan","numberOfAyahs":7,"ayahs":[{"number":1,"text":"\ufeff\u0628\u0650\u0633\u0652\u0645\u0650 \u0627\u0644\u0644\u0651\u064e\u0647\u0650 \u0627\u0644\u0631\u0651\u064e\u062d\u0652\u0645\u064e\u0670\u0646\u0650 \u0627\u0644\u0631\u0651\u064e\u062d\u0650\u064a\u0645\u0650","numberInSurah":1,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":2,"text":"\u0627\u0644\u0652\u062d\u064e\u0645\u0652\u062f\u064f \u0644\u0650\u0644\u0651\u064e\u0647\u0650 \u0631\u064e\u0628\u0651\u0650 \u0627\u0644\u0652\u0639\u064e\u0627\u0644\u064e\u0645\u0650\u064a\u0646\u064e","numberInSurah":2,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":3,"text":"\u0627\u0644\u0631\u0651\u064e\u062d\u0652\u0645\u064e\u0670\u0646\u0650 \u0627\u0644\u0631\u0651\u064e\u062d\u0650\u064a\u0645\u0650","numberInSurah":3,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":4,"text":"\u0645\u064e\u0627\u0644\u0650\u0643\u0650 \u064a\u064e\u0648\u0652\u0645\u0650 \u0627\u0644\u062f\u0651\u0650\u064a\u0646\u0650","numberInSurah":4,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":5,"text":"\u0625\u0650\u064a\u0651\u064e\u0627\u0643\u064e \u0646\u064e\u0639\u0652\u0628\u064f\u062f\u064f \u0648\u064e\u0625\u0650\u064a\u0651\u064e\u0627\u0643\u064e \u0646\u064e\u0633\u0652\u062a\u064e\u0639\u0650\u064a\u0646\u064f","numberInSurah":5,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":6,"text":"\u0627\u0647\u0652\u062f\u0650\u0646\u064e\u0627 \u0627\u0644\u0635\u0651\u0650\u0631\u064e\u0627\u0637\u064e \u0627\u0644\u0652\u0645\u064f\u0633\u0652\u062a\u064e\u0642\u0650\u064a\u0645\u064e","numberInSurah":6,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":7,"text":"\u0635\u0650\u0631\u064e\u0627\u0637\u064e \u0627\u0644\u0651\u064e\u0630\u0650\u064a\u0646\u064e \u0623\u064e\u0646\u0652\u0639\u064e\u0645\u0652\u062a\u064e \u0639\u064e\u0644\u064e\u064a\u0652\u0647\u0650\u0645\u0652 \u063a\u064e\u064a\u0652\u0631\u0650 \u0627\u0644\u0652\u0645\u064e\u063a\u0652\u0636\u064f\u0648\u0628\u0650 \u0639\u064e\u0644\u064e\u064a\u0652\u0647\u0650\u0645\u0652 \u0648\u064e\u0644\u064e\u0627 \u0627\u0644\u0636\u0651\u064e\u0627\u0644\u0651\u0650\u064a\u0646\u064e","numberInSurah":7,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false}],"edition":{"identifier":"quran-simple","language":"ar","name":"Simple","englishName":"Simple","format":"text","type":"quran"}}}
+
+console.log(Quran.data.name);
+console.log(Quran.data.ayahs[0].text);
+console.log(Quran.data.ayahs[1].text);
+console.log(Quran.data.ayahs[2].text);
+console.log(Quran.data.ayahs[3].text);
+console.log(Quran.data.ayahs[4].text);
+console.log(Quran.data.ayahs[5].text);*/
+
+/*var nilai = 3;
+if (nilai >= 6){
+    console.log('Selamat anda lulus, dengan nilai memuaskan')
+} else if (nilai >= 6){
+    console.log('Selamat anda lulus, dengan nilai memuaskan')
+}else {
+    console.log('Belajar Lagi Ya')
+    alert("Belajar Lagi Ya")
+}
+
+var jam = 19;
+if (jam >=1 && jam<=11) {
+    alert('Selamat Pagi')
+}else if (jam >=12 && jam<=15) {
+    alert('Selamat Siang')
+}else if (jam >=16 && jam<=18) {
+    alert('Selamat Sore')
+}else {
+    alert('Selamat Malam')
+}
+
+var buah = 'jeruk';
+switch (buah){
+    case "jeruk":
+        alert ('Warna buah jeruk itu orange/kuning');
+        break;
+    case "apel":
+        alert ('Warna buah jeruk itu merah/kuning');
+        break;
+    default:
+        alert("Nama buah tidak dikenal");
+}
+var hari = '';
+switch (hari){
+    case "1":
+        alert ('Hari Senin');
+        break;
+    case "2":
+        alert ('Hari Selasa');
+        break;
+    case "3":
+        alert ('Hari Rabu');
+        break;
+    case "4":
+        alert ('Hari Kamis');
+        break;
+    case "5":
+        alert ("Hari Jum'at");
+        break;
+    case "6":
+        alert ('Hari Sabtu');
+        break;
+    case "7":
+        alert ('Hari Minggu');
+        break;
+    default:
+        alert("Hari Libur");
+}
+
+for ( i=1; i <= 10; i++) {
+    console.log( 'Perulangan ke-' + i );
+    }
+
+var i = 0;
+while ( i <= 10 ) {
+    console.log( 'Perulangan ke-' + i );
+    i++;
+}
+
+var buah = ["Apel","Jeruk","Mangga","Delima"];
+for ( i=0; i <buah.length ; i++) {
+    console.log ( buah [i] );
+}
+
+
+function nama(namaAnda, alamat) {
+    alert("Selamat Malam Tuan "+namaAnda);
+    return namaAnda;
+}
+
+function greet() {
+    var inputName = document.getElementById('nama').value;
+    var inputEmail = document.getElementById('alamatEmail').value;
+    alert('Selamat Datang '+ inputName + " "+ inputEmail);
+}
+var btn =document.getElementById('btn');
+btn.addEventListener('click',greet);*/
+
+
+function greeting(event){
+	var nama = event.target.textContent;
+	return alert("Halo " + nama);
+}
+
+var app = document.getElementsByClassName('greet');
+
+for (var i = 0; i < app.length; i++) {
+	app[i].addEventListener('click' , greeting);
+}
